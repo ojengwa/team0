cc864d7aaf8503e0c39ab20558f1d42b7e04e1e8
Fadejimi <adegbulugbefadejimi@gmail.com>
2015-06-17 19:26:24 +0100

    Updated with a Google sign in form yet to send the data using JSON

---
+++ tests/config.js
@@ -0,0 +1,4 @@
+var clientData = {}
+clientData.clientid = 1029543985020-npv8fv6lo5kuoira7ck17bfuu5m0m8up.apps.googleusercontent.com
+clientData.email = 1029543985020-npv8fv6lo5kuoira7ck17bfuu5m0m8up@developer.gserviceaccount.com
+clientData.secret = Ps3VeTxWhWboqm92VuhlKpKB
