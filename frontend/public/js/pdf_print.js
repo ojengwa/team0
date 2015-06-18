707a9fb144b864e35a8bc261880d962da8f1da31
Faiçal Tchirou <ftchirou@gmail.com>
2015-06-18 10:05:25 +0100

    add a button to the viewer to go back to the homepage

---
+++ frontend/public/js/pdf_print.js
@@ -0,0 +1,38 @@
+function loading(filename) {
+  var url = "pdf/web/viewer.html";
+  var add = "?file=".concat(encodeURIComponent(filename));
+  var res = encodeURIComponent(add);
+  var page = url.concat(add);
+
+  window.location.href=page;
+}
+
+// Attach a submit handler to the form
+$( "#submit" ).click(function( event ) {
+    // Stop form from submitting normally
+    event.preventDefault();
+     
+    var data = {};
+    data["url"] = $("#urltext").val();
+    addData(data);
+});
+
+// send the data using post to the app's url
+function addData(data){
+    $.ajax({
+        type: "POST",
+        //url: "http://aqueous-atoll-7901.herokuapp.com/v1/files",
+        url: "http://127.0.0.1:5000/v1/files",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: "json",
+        success: function (result, status) {
+            processResponse(result);
+        }
+    });
+}
+
+function processResponse(data){
+    loading(data.url);
+}
